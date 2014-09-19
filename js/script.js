2025c8d88e02cbf71e82895f9e6d980f658f3f56
Chris Dees <christandees@gmail.com>
2014-09-18 19:54:13 -0700

    .02

--- js/script.js
+++ js/script.js
@@ -1,45 +1,65 @@
-var ppictureSource, destinationType;
-document.addEventListener('deviceready',loaded, false);
+var pictureSource, destinationType;
+
+document.addEventListener('deviceready', loaded, false);
+
 function loaded(){
 	pictureSource = navigator.camera.PictureSourceType.CAMERA;
-	destinationType = navigator.camera.DestinationType.DATA_URL;	
+	destinationType = navigator.camera.DestinationType.DATA_URL;
 }
+
 function capturePhoto(){
 	navigator.camera.getPicture(getPhoto, onFail, {
-			quality:80, destinationType:destinationType,
-			sourceType: pictureSource,
-			allowEdit:true
-		});
-		
-		
+		quality:80,
+		destinationType:destinationType,
+		sourceType:pictureSource,
+		allowEdit:true
+	});
 }
 function getPhoto(imageData){
 	var smallImage = document.getElementById('selfie');
 	smallImage.style.display = 'block';
-	smallImage.src = "data:image/jpeg;base64,"+imageData
+	smallImage.src = "data:image/jpeg;base64,"+imageData;
 }
-function onFail(){
-	alert('failed because: ' +message);
+function onFail(message){
+	alert('Failed due to: '+message);
+}
 
-}
 $(document).ready(function(e){
 	$('html').addClass('js');
 	
 	$('[data-role=page]').on('mouseover', '[data-icon]', function(e){
 		$(this).addClass('active');
 	});
-	$('[data-role=page]').on('mouseout','[data-icon]', function(e){
+	$('[data-role=page]').on('mouseout', '[data-icon]', function(e){
 		$(this).removeClass('active');
 	});
 	
+	$('[data-tab=explore]').on('click', function(e){
+		$('#explore').show();
+		$('#share').hide();
+		$('#likes').hide();
+		$('header h1').text('Explore');
+	});
+	$('[data-tab=share]').on('click', function(e){
+		$('#explore').hide();
+		$('#share').show();
+		$('#likes').hide();
+		$('header h1').text('Share');
+	});
+	$('[data-tab=likes]').on('click', function(e){
+		$('#explore').hide();
+		$('#share').hide();
+		$('#likes').show();
+		$('header h1').text('Likes');
+	});
 	
 	$.ajax({
-		url:'http://cdees.catstudents.com/snapit/data.php',
+		url:'http://jmelsom.catstudents.com/snapIt/data.php',
 		success: function(data){
-		$('#explore ul').append(data);
+			$('#explore ul').append(data);
 		}
-		
-		});
-	
-	
+	});
+	$('#likethis').on('click', function(e){
+		$('#likes ul').append().parent().parent().parent();
+	});
 });
