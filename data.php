b6fa306b78e055404be376da152f6c35ca84225f
Chris Dees <christandees@gmail.com>
2014-09-19 01:43:52 -0700

    .06

--- data.php
+++ data.php
@@ -1,13 +1,21 @@
-<?php for($i=0 ; $i < 10; $i++){ ?>
+<?php
 
-<li>
-	<figure>
-		<img src="http://lorempixel.com/300/20<?=$i?>/people" alt="selfie">
-		<figcaption>
-			<a data-icon="flag" data-iconpos="notext" data-pos="left" id="flagthis">Flag</a>
-			<a data-icon="star" data-iconpos="right" data-pos="right" id="likethis">9,999</a>
-		</figcaption>
-	</figure>
-</li>
+for ($i = 0; $i < 10; $i++){
+	
 
-<?php } ?>
+
+
+?>
+
+				<li>
+					<figure>
+						<img src="http://lorempixel.com/300/20<?=$i?>" alt="selfie">
+						<figcaption>
+							<a data-icon="flag" data-iconpos="notext" data-pos="left">Flag</a>
+							<a data-icon="star" data-iconpos="right" data-pos="right">9,999</a>
+						</figcaption>
+					</figure>
+				</li>
+<?php
+}
+?>
